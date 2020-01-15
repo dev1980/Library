c954f279396a667baef6c97c1857fbc13d49cff9
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-01-15 10:24:24 -0600

    add toggle functionality to button status

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 
-let myLibrary = JSON.parse(window.localStorage.getItem('library'));
+let myLibrary = JSON.parse(window.localStorage.getItem("library"));
 if (myLibrary == null) {
   myLibrary = [];
 }
@@ -21,32 +21,32 @@ class UI {
   }
 
   static addBookToList(book, index) {
-    const list = document.getElementById('tableBody');
+    const list = document.getElementById("tableBody");
     const row = `<tr>
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.pages}</td>
-    <td><button class="btn btn-primary">${book.type}</button></td>
+    <td><button id="toggle-btn" class="btn btn-primary" onclick="toggleStatus()">${book.type}</button></td>
     <td><a href='#' data-index= ${index} class='btn btn-danger btn-sm delete'>X</a></td> 
     </tr>`;
     list.innerHTML += row;
   }
 
   static updateLocalStorage(array) {
-    window.localStorage.setItem('library', JSON.stringify(array));
+    window.localStorage.setItem("library", JSON.stringify(array));
   }
 
   static deleteBook(el) {
-    if (el.classList.contains('delete')) {
+    if (el.classList.contains("delete")) {
       el.parentElement.parentElement.remove();
-      const book = el.getAttribute('data-index');
+      const book = el.getAttribute("data-index");
       myLibrary.splice(book, 1);
       UI.updateLocalStorage(myLibrary);
     }
   }
 
   static clearFields() {
-    const formBook = document.getElementById('form-book');
+    const formBook = document.getElementById("form-book");
     formBook.reset();
   }
 
@@ -59,25 +59,27 @@ class UI {
   }
 
   static showAlert(type, displayMessage) {
-    const message = document.getElementById('message');
+    const message = document.getElementById("message");
     message.innerHTML = `<div class='alert alert-${type} alert-dismissible fade show' role='alert'>
                                 <strong>Message:</strong>${displayMessage}
                                 <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
                                 <span aria-hidden='true'>&times;</span>
                                 </button>
                             </div>`;
-    setTimeout(() => { message.innerHTML = ''; }, 5000);
+    setTimeout(() => {
+      message.innerHTML = "";
+    }, 5000);
   }
 }
 
-document.addEventListener('DOMContentLoaded', UI.displayBooks());
+document.addEventListener("DOMContentLoaded", UI.displayBooks());
 
-document.getElementById('form-book').addEventListener('submit', (e) => {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const read = document.getElementById('read');
-  const unread = document.getElementById('unread');
+document.getElementById("form-book").addEventListener("submit", e => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read");
+  const unread = document.getElementById("unread");
   const type = read.checked ? read.value : unread.value;
 
   // Instatiate Book
@@ -89,14 +91,23 @@ document.getElementById('form-book').addEventListener('submit', (e) => {
     UI.addBookToList(book);
     // Clear Field
     UI.clearFields();
-    UI.showAlert('success', 'You have book has been added book succesefully.');
+    UI.showAlert("success", "You have book has been added book succesefully.");
   } else {
-    UI.showAlert('danger', 'Sorry you can not add this book Please fill the form.');
+    UI.showAlert(
+      "danger",
+      "Sorry you can not add this book Please fill the form."
+    );
   }
 
   e.preventDefault();
 });
 // Events : To remove the book from the UI
-document.querySelector('#tableBody').addEventListener('click', (e) => {
+document.querySelector("#tableBody").addEventListener("click", e => {
   UI.deleteBook(e.target);
 });
+
+function toggleStatus() {
+  let btnVal = document.getElementById("toggle-btn").textContent;
+  document.getElementById("toggle-btn").textContent =
+    btnVal == "Read" ? "Unread" : "Read";
+}
